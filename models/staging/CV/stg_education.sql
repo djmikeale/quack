a4302e30f4dd5939edc7bf8fb26c83fa203aaf95
Mikael Thorup <mika@lunar.app>
2023-08-13 23:13:59 +0200

    run sqlfmt on all models

--- models/staging/CV/stg_education.sql
+++ models/staging/CV/stg_education.sql
@@ -1,23 +1,23 @@
 with
-    source as (
-        select * from {{ ref('raw_cv') }}
-
-    ),
+    source as (select * from {{ ref("raw_cv") }}),
 
     clean as (
         select
-            {% for column in ['type','institution','location','title','start_date','end_date','details'] %}
-            unnest(education).{{column}} as {{column}}, --loving how duckdb doesn't care about ending a sql statement with a comma, lol.
+            {% for column in [
+                "type",
+                "institution",
+                "location",
+                "title",
+                "start_date",
+                "end_date",
+                "details",
+            ] %}
+            unnest(education).{{ column }} as {{ column }},  -- loving how duckdb doesn't care about ending a sql statement with a comma, lol.
             {% endfor %}
         from source
     ),
 
-    transform as (
-        select
-            hash(end_date) as job_id,
-            *
-        from clean
-    )
+    transform as (select hash(end_date) as job_id, * from clean)
 
 select *
 from transform
